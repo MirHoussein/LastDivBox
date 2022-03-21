bfcf61f77a620f9b1bed6c9771bfc52261e7a103
MirHoussein <elekberli.mirhuseyn@mail.ru>
2022-03-21 23:27:15 +0400

    lastbox

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+var documentHeight = document.documentElement.clientHeight;
+var documentWidth = document.documentElement.clientWidth;
 // Random Color
+
 
 function randomColors() {
     const colors = "0123456789ABCDEF";
 
-let color = '#';
+    let color = '#';
 
 
-for (let i = 0; i < 6; i++)
-   color += colors[(Math.floor(Math.random() * 16))];
+    for (let i = 0; i < 6; i++)
+        color += colors[(Math.floor(Math.random() * 16))];
 
 
-   return color;
+    return color;
 }
-
-
 
 // Button create
 const btn = document.querySelector('button')
@@ -21,68 +22,36 @@ btn.addEventListener('click', divYarat)
 
 
 function divYarat() {
-    
-    
-    
 
-    
     // Create Div 
     const newDiv = document.createElement('div')
+    const yeniDiv=Math.floor(Math.random()*80+20)
+    const x = Math.round(Math.random() * (documentWidth)-yeniDiv);
+    const y = Math.round(Math.random() * (documentHeight) -yeniDiv)
+    const sizeWidth = Math.floor(Math.random() * 100);
+    const sizeHeight = Math.floor(Math.random() * 100);
+    newDiv.style.size=sizeWidth+'px'
+    newDiv.style.size=sizeHeight+'px'   
+    newDiv.style.left = x+'px';
+    newDiv.style.top = y+'px'
+    newDiv.style.width = '40px';
+    newDiv.style.height ='55px';
     
+    newDiv.style.position = 'absolute';
+    newDiv.style.backgroundColor = `${randomColors()}`;
 
-   
-   
-    const x = Math.round(Math.random()*880+1)
-    const y = Math.round(Math.random()*880+1)
-
-    const sizeWidth=Math.floor(Math.random()*100+'px');
-    const sizeHeight=Math.floor(Math.random()*100+'px');
-    newDiv.style.width=sizeWidth
-    newDiv.style.height=sizeHeight;
-    
-   
-    newDiv.style.top=`${x}px`
-    newDiv.style.left=`${y}px`;
-    newDiv.style.width=`170px`;
-    newDiv.style.height='145px';
-    newDiv.style.position='relative';
-    newDiv.style.position='absolute';
-  
-    
-    newDiv.style.backgroundColor=`${randomColors()}`;
-   
     document.body.appendChild(newDiv);
-   
-  
-    
-
 
     // Delete Div
 
-    newDiv.addEventListener('click',()=>{
+    newDiv.addEventListener('click', () => {
         newDiv.remove();
 
-
-       
     })
 
-   
-    
+}
 
-    
-    
 
-    
-   
-   
-   
 
-    
-       
-   
-    }
-    
-    
 
- 
-  
+
